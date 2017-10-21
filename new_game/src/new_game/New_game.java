26436360a61b8240253088bed13a6a47e0125210
Alpabox11 <32977783+Alpabox11@users.noreply.github.com>
2017-10-21 14:24:05 +0530

    Update new game
    Added new feature

--- new_game/src/new_game/New_game.java
+++ new_game/src/new_game/New_game.java
@@ -17,4 +17,29 @@ public class New_game {
         game obj = new game();
         obj.the_game();
     }
+     public static void main(String[] args) {
+        game obj = new game();
+        obj.the_game();
+    }
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new getLocation(MainActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+                   
+                        objclass = new getData();
+                        objclass.setUp("",latitude, longitude, 0);
+                        objclass.execute().get();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+
 }
